f81848480bda4a0d6155bb7deb6a6ce8f3df7e48
IK3CHI <ikechigreat@gmail.com>
2024-09-09 13:06:38 +0100

    Created a database resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { dataSourceOptions } from 'src/database/db/data-source';
+import { dataSourceOptions } from 'src/database/data-source';
 import { AuthModule } from './auth/auth.module';
-import { DatabaseModule } from './database/database.module';
+
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions), AuthModule, DatabaseModule],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
